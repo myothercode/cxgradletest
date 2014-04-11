a6d75db760e5c385e546999d1a044a4e4b8c93d9
caixu23 <caixu23@gmail.com>
2014-04-11 13:40:47 +0800

    rrrrrrrrrrrrrrrr

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * Created by chace.cai on 14-4-4.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+        Test1 test1=new Test1();
+        test1.test1();
+    }
+}
---
+++ test1/src/main/java/com/Test1.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by chace.cai on 14-4-4.
+ */
+public class Test1 {
+    public void test1(){
+        System.out.println("test1");
+    }
+}
